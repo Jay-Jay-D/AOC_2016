f5842bf827488cff53520212af3d47c8e3a756e1
Jay-Jay-D <jjdambrosio@gmail.com>
2022-06-20 22:06:39 -0300

    Day one complete.

---
+++ Tests/DayOneTests.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using DayOne;
+
+namespace DayOneTests;
+
+public class DayOneTests
+{
+    [Theory]
+    [InlineData("R2, L3", 5)]
+    [InlineData("R2, R2, R2", 2)]
+    [InlineData("R5, L5, R5, R3", 12)]
+    public void TestEstimateDistance(string instructions, int expectedDistance)
+    {
+        var noTimeForATaxicab = new DayOne.NoTimeForATaxicab();
+        Assert.Equal(expectedDistance, noTimeForATaxicab.FollowInstructions(instructions));
+
+    }
+
+    [Fact]
+    public void TestFindEasterBunnyHQ()
+    {
+        // Arrange
+        var instructions = "R8, R4, R4, R8";
+        var expectedDistance = 4;
+        var noTimeForATaxicab = new DayOne.NoTimeForATaxicab();
+        // Act
+        var distance = noTimeForATaxicab.FollowInstructions(instructions);
+        //var actualDistance = DayOne.NoTimeForATaxicab.GetDistanceFromCoords(noTimeForATaxicab.EasterBunnyHQ);
+        // Assert
+        noTimeForATaxicab.EasterBunnyHQ.Should().NotBeNull();
+        Assert.Equal(expectedDistance, noTimeForATaxicab.EasterBunnyHQ.GetDistanceToOrigin());
+
+    }
+}
