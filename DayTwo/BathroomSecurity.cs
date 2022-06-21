f626e9d1e3d941d71b5e2345b4f9eec7a55d1eeb
Jay-Jay-D <jjdambrosio@gmail.com>
2022-06-20 22:55:48 -0300

    Day two test red.

---
+++ DayTwo/BathroomSecurity.cs
@@ -0,0 +1,26 @@
+using DayOne;
+namespace DayTwo;
+
+
+public class BathroomSecurity
+{
+    public BathroomSecurity()
+    {
+        StartingPoint = new Point(1, 1);
+    }
+
+    public BathroomSecurity(Point startingPoint)
+    {
+        StartingPoint = startingPoint;
+    }
+
+    public int DecodeLine(string instruction)
+    {
+        return 0;
+    }
+
+
+    public Point StartingPoint { get; }
+
+
+}
---
+++ Tests/DayTwoTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+using DayOne;
+using DayTwo;
+
+namespace DayTwoTests;
+public class DayTwoTests
+{
+
+    public static IEnumerable<object[]> InstructionLineCases =>
+        new List<object[]>
+        {
+            new object[] { new Point(1,1),"ULL", 1 },
+            new object[] { new Point(0,2),"RRDDD", 9 },
+            new object[] { new Point(0,2),"LURDL", 8 },
+            new object[] { new Point(1,0),"UUUUD", 5 },
+
+        };
+
+
+    [Theory]
+    [MemberData(nameof(InstructionLineCases))]
+    public void TestDecodeNumber(Point startingPoint, string instruction, int expectedNumber)
+    {
+        // Arrange
+        var bathroomSecurity = new DayTwo.BathroomSecurity(startingPoint);
+        // Act
+        var decodedNumber = bathroomSecurity.DecodeLine(instruction);
+        // Assert
+        decodedNumber.Should().Be(expectedNumber);
+
+    }
+}
