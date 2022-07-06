74f76223d6faaac18bbfff07c94f14061616b7d0
Jay-Jay-D <jjdambrosio@gmail.com>
2022-07-06 10:44:06 -0300

    Day Eight done.

--- DayEight/Program.cs
+++ DayEight/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DayEight;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var instructions = File.ReadAllLines(@"Input/DayEight.txt");
+        var screen = new AuthenticationScreen(6, 50);
+        foreach (var instruction in instructions)
+        {
+            screen.Proccess(instruction);
+        }
+
+        Console.WriteLine($"Part 1: there are {screen.LitPixels()} lit pixels.");
+        Console.WriteLine("Part2:");
+        screen.ShowScreen();
+    }
+}
