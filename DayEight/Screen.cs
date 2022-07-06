626a85f7a0392dc3d429ee1903fef7fe826ec6c7
Jay-Jay-D <jjdambrosio@gmail.com>
2022-07-06 10:36:22 -0300

    Small refactoring.

--- DayEight/Screen.cs
+++ DayEight/Screen.cs
@@ -35,35 +35,45 @@ namespace DayEight
                 var rectSize = parts[1].Split('x');
                 var wide = int.Parse(rectSize[0]);
                 var tall = int.Parse(rectSize[1]);
-                for (int row = 0; row < tall; row++)
-                {
-                    for (int col = 0; col < wide; col++)
-                    {
-                        _screen[row, col] = 1;
-                    }
-                }
+                RectInstrustion(wide, tall);
             }
             else
             {
                 var isRotateRow = parts[1] == "row";
-                var arraySize = isRotateRow ? Columns : Rows;
                 var position = int.Parse(parts[2].Split("=")[1]);
                 var shift = int.Parse(parts.Last());
-                var originalArray = Enumerable.Range(0, arraySize)
-                                              .Select(idx => isRotateRow ? _screen[position, idx] : _screen[idx, position])
-                                              .ToArray();
-                for (var idx = 0; idx < arraySize; idx++)
-                {
-                    var shiftedIdx = (idx + shift) % arraySize;
-                    if (isRotateRow)
-                    {
-                        _screen[position, shiftedIdx] = originalArray[idx];
-                    }
-                    else
-                    {
-                        _screen[shiftedIdx, position] = originalArray[idx];
+                RotateInstruction(isRotateRow, position, shift);
+            }
+        }
 
-                    }
+        private void RotateInstruction(bool isRotateRow, int position, int shift)
+        {
+            var arraySize = isRotateRow ? Columns : Rows;
+            var originalArray = Enumerable.Range(0, arraySize)
+                                          .Select(idx => isRotateRow ? _screen[position, idx] : _screen[idx, position])
+                                          .ToArray();
+            for (var idx = 0; idx < arraySize; idx++)
+            {
+                var shiftedIdx = (idx + shift) % arraySize;
+                if (isRotateRow)
+                {
+                    _screen[position, shiftedIdx] = originalArray[idx];
+                }
+                else
+                {
+                    _screen[shiftedIdx, position] = originalArray[idx];
+
+                }
+            }
+        }
+
+        private void RectInstrustion(int wide, int tall)
+        {
+            for (int row = 0; row < tall; row++)
+            {
+                for (int col = 0; col < wide; col++)
+                {
+                    _screen[row, col] = 1;
                 }
             }
         }
