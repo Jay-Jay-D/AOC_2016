12f2664fb318113a4135aedaf01f9c1437749ea0
Jay-Jay-D <jjdambrosio@gmail.com>
2022-07-08 13:02:33 -0300

    Day nine, first test green.

--- DayNine/ExplosivesInCyberspace.cs
+++ DayNine/ExplosivesInCyberspace.cs
@@ -1,9 +1,43 @@
+using System.Text;
+
 namespace DayNine;
 
 public class ExplosivesInCyberspace
 {
     public static object Decompress(string compressed)
     {
-        throw new NotImplementedException();
+        var idx = 0;
+        var charsStart = 0;
+        var charsCountToRepeat = 0;
+        var repetitions = 0;
+        var decompressed = new StringBuilder();
+        while (idx < compressed.Length)
+        {
+
+            if (compressed[idx] == ' ')
+            {
+                idx++;
+                continue;
+            }
+            if (compressed[idx] == '(')
+            {
+                idx++; // the '(' character
+                var marker = new string(compressed.Skip(idx).TakeWhile(c => c != ')').ToArray());
+                idx += marker.Length + 1; // the ')' character
+                charsStart = idx;
+                var markerParts = marker.Split("x");
+                charsCountToRepeat = int.Parse(markerParts[0]);
+                repetitions = int.Parse(markerParts[1]);
+                var charsToRepeat = new string(compressed.Skip(idx).Take(charsCountToRepeat).ToArray());
+                for (var r = 0; r < repetitions; r++)
+                {
+                    decompressed.Append(charsToRepeat);
+                }
+                idx += charsCountToRepeat;
+                continue;
+            }
+            decompressed.Append(compressed[idx++]);
+        }
+        return decompressed.ToString();
     }
 }
