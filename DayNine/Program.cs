6518c2007de76b843e0461cf409bde7b6ba9ca92
Jay-Jay-D <jjdambrosio@gmail.com>
2022-07-08 19:35:22 -0300

    Day nine done.

--- DayNine/Program.cs
+++ DayNine/Program.cs
@@ -7,5 +7,7 @@ public class Program
         var compressed = File.ReadAllText(@"Input/DayNine.txt");
         var decompressed = ExplosivesInCyberspace.Decompress(compressed);
         Console.WriteLine($"Part 1: the decompressed length is {decompressed.Length}");
+        var decompresseLenght = ExplosivesInCyberspace.GetDecompressLenght(compressed);
+        Console.WriteLine($"Part 2: the decompressed length is {decompresseLenght}");
     }
 }
