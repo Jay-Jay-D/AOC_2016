ab050028f66c88dbfa6c9d09fe5ff48c8793e673
Jay-Jay-D <jjdambrosio@gmail.com>
2022-07-08 17:15:01 -0300

    Day nine part one done.

--- DayNine/Program.cs
+++ DayNine/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DayNine;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var compressed = File.ReadAllText(@"Input/DayNine.txt");
+        var decompressed = ExplosivesInCyberspace.Decompress(compressed);
+        Console.WriteLine($"Part 1: the decompressed length is {decompressed.Length}");
+    }
+}
