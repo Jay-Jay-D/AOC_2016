4d8ac91ab7c26f5755e8ff0a0d421331775f6d36
JJD <jjdambrosio@gmail.com>
2022-09-14 08:49:39 -0300

    Part 1 green!

--- DayTen/Program.cs
+++ DayTen/Program.cs
@@ -4,25 +4,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-        var instructions = new[]
-            {
-            "value 5 goes to bot 2",
-            "bot 2 gives low to bot 1 and high to bot 0",
-            "value 3 goes to bot 1",
-            "bot 1 gives low to output 1 and high to bot 0",
-            "bot 0 gives low to output 2 and high to output 0",
-            "value 2 goes to bot 2",
-        };
-
-        var expectedBots = new[]{
-            new Bot(2,1,0),
-            new Bot(1,int.MinValue,0),
-            new Bot(0,int.MinValue,int.MinValue)
-        };
+        var instructions = File.ReadAllLines(@"Input/DayTen.txt");
         var balanceBots = new BalanceBots(instructions);
-
-        // When
         balanceBots.InitializeBots();
+        balanceBots.Run();
+        Console.WriteLine("Part 1: the bot that is responsible for comparing value-61 microchips " +
+                          $"with value-17 microchips is {balanceBots.WhichBotCompared(17, 61)}.");
     }
 }
