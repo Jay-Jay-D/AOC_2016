9d65278c809c2980cca9e0e76cbb1d0cc3a51cd8
JJD <jjdambrosio@gmail.com>
2022-09-15 10:34:49 -0300

    Day eleven first commit
    elevator and load logic WIP

---
+++ DayEleven/Elevator.cs
@@ -0,0 +1,50 @@
+namespace DayEleven;
+
+public class Elevator
+{
+    public int Floor { get; private set; }
+    public bool IsEmpty { get; set; } = true;
+
+    public Elevator(int floor)
+    {
+        Floor = floor;
+    }
+
+    public bool Move(int v)
+    {
+        return !IsEmpty;
+    }
+
+    public bool Load((string Type, string Material) load)
+    {
+        IsEmpty = false;
+        return false;
+    }
+
+    public void Unload((string Type, string Material) load)
+    {
+        IsEmpty = true;
+    }
+
+    public bool Load((string Type, string Material)[] load)
+    {
+        if (load.Length > 2)
+        {
+            return false;
+        }
+        return IsLoadCompatible(load);
+    }
+
+    public void Unload((string Type, string Material)[] load)
+    {
+        IsEmpty = true;
+    }
+
+    bool IsLoadCompatible((string Type, string Material)[] load)
+    {
+        if (load.Length==1) return true;
+        if (load[0].Material == load[1].Material) return true;
+        if (load[0].Type == load[1].Type) return true;
+        return false;
+    }
+}
---
+++ Tests/DayElevenTests.cs
@@ -0,0 +1,101 @@
+using DayEleven;
+
+namespace DayElevenTests;
+
+public class DayElevenTests
+{
+    [Fact]
+    public void ElevatorInitialStateIsEmpty()
+    {
+        // Given
+        // When
+        var elevator = new Elevator(0);
+    
+        // Then
+        elevator.IsEmpty.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ElevatorKnowsItsFloor()
+    {
+        // Given
+        // When
+        var elevator = new Elevator(1);
+    
+        // Then
+        elevator.Floor.Should().Be(1);
+    }
+
+    [Fact]
+    public void EmptyElevatorCannotMove()
+    {
+        // Given
+        // When
+        var elevator = new Elevator(1);
+    
+        // Then
+        elevator.Move(1).Should().BeFalse();
+    }
+
+    [Fact]
+    public void AfterLoadingAnObjectElevatorIsNotEmpty()
+    {
+        // Given
+        var elevator = new Elevator(1);
+        var generator = (Type:"generator", Material: "hydrogen");
+    
+        // When
+        elevator.Load(generator); 
+    
+        // Then
+        elevator.IsEmpty.Should().BeFalse();
+    }
+
+    [Fact]
+    public void AfterLoadingAndUnloadignElevatorIsEmpty()
+    {
+        // Given
+        var elevator = new Elevator(1);
+        var generator = (Type:"generator", Material: "hydrogen");
+        elevator.Load(generator);
+
+        // When
+        elevator.Unload(generator);
+    
+        // Then
+        elevator.IsEmpty.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ElevatorCanLoadUpToTwoObjects()
+    {
+        // Given
+        var elevator = new Elevator(1);
+        var generator = (Type:"generator", Material: "hydrogen");
+        var chip = (Type:"microchip", Material: "hydrogen");
+        var load = new[]{generator, chip};
+        
+         // When
+        elevator.Load(load).Should().BeTrue();
+       
+        // Then
+        elevator.Unload(load);
+        elevator.IsEmpty.Should().BeTrue();
+        load = new[]{generator, chip, chip};
+        elevator.Load(load).Should().BeFalse();
+    }
+
+    [Fact]
+    public void CannotLoadGeneratorWithMicrochipOfDifferentMaterial()
+    {
+        // Given
+        var elevator = new Elevator(1);
+        var generator = (Type:"generator", Material: "hydrogen");
+        var chip = (Type:"microchip", Material: "lithium");
+        var load = new[]{generator, chip};
+        
+        // When
+        // Then
+        elevator.Load(load).Should().BeFalse();
+    }
+}
